c7ad939dcaed56b2b1c6adceb9d36dea45e45e47
Andreas Wittig <andreas@widdix.de>
2021-03-16 13:55:16 +0100

    fixing test

--- examples/fargate-alb-auth-cognito/test/test.js
+++ examples/fargate-alb-auth-cognito/test/test.js
@@ -4,7 +4,10 @@ const cfntest = require('@cfn-modules/test');
 test.serial('example', async t => {
   const stackName = cfntest.stackName();
   try {
-    t.log(await cfntest.createStack(`${__dirname}/../example.yml`, stackName, {}));
+    t.log(await cfntest.createStack(`${__dirname}/../example.yml`, stackName, {
+      "HostedZoneId": "Z08057981AMLWUWNFCQLS",
+      "SubDomainNameWithDot": "fargate-alb-auth-cognito."
+    }));
     const outputs = await cfntest.getStackOutputs(stackName);
     t.log(outputs);
     const res = await cfntest.probeHttpGet(outputs.Url);
