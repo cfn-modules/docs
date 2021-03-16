dbadce1e418280490aefe94145575b9253393117
Andreas Wittig <andreas@widdix.de>
2021-03-16 11:27:20 +0100

    Adding ALB + Cognito example

---
+++ examples/fargate-alb-auth-cognito/test/test.js
@@ -0,0 +1,17 @@
+const test = require('ava');
+const cfntest = require('@cfn-modules/test');
+
+test.serial('example', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/../example.yml`, stackName, {}));
+    const outputs = await cfntest.getStackOutputs(stackName);
+    t.log(outputs);
+    const res = await cfntest.probeHttpGet(outputs.Url);
+    t.is(res.headers.server, 'Apache/2.4.23 (Unix)');
+    t.true(res.data.includes('It works!'));
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
